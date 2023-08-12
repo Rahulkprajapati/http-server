5b9f3a13f525f87ac345438bd5f58bfa4fc724c3
Rahulkrajapati <rahulkpkprajapati147@gmail.com>
2023-08-12 23:51:10 +0530

    rewritten

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
-// package main
+package main
 
-// import (
-//     "fmt"
-//     "net/http"
-// )
+import (
+    "fmt"
+    "net/http"
+)
 
-// type CounterHandler struct {
-//     counter int
-// }
+type CounterHandler struct {
+    counter int
+}
 
-// func (ct *CounterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//     fmt.Println(ct.counter)
-//     ct.counter++
-//     fmt.Fprintln(w, "Counter:", ct.counter)
-// }
+func (ct *CounterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(ct.counter)
+    ct.counter++
+    fmt.Fprintln(w, "Counter:", ct.counter)
+}
 
-// func main() {
+func main() {
 
-//     th := &CounterHandler{counter: 0}
-//     http.Handle("/count", th)
-//     http.ListenAndServe(":8080", nil)
-// }
+    th := &CounterHandler{counter: 0}
+    http.Handle("/count", th)
+    http.ListenAndServe(":8080", nil)
+}
